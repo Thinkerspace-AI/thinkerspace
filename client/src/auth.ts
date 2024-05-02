07e47846c22f2132a2657747f4cb1b0c847cabec
Marius Barcenas <mariusulyzses.barcenas@gmail.com>
2024-05-02 16:07:18 +0800

    feat: add Google as oauth provider

--- client/src/auth.ts
+++ client/src/auth.ts
@@ -1,10 +1,16 @@
 import { SvelteKitAuth } from "@auth/sveltekit";
 import GitHub from "@auth/sveltekit/providers/github";
-import { GITHUB_ID, GITHUB_SECRET } from "$env/static/private";
+import Google from "@auth/sveltekit/providers/google";
+import { GITHUB_ID, GITHUB_SECRET, AUTH_GOOGLE_ID, AUTH_GOOGLE_SECRET } from "$env/static/private";
 
 export const { handle, signIn, signOut } = SvelteKitAuth({
-  providers: [GitHub({
-    clientId: GITHUB_ID,
-    clientSecret: GITHUB_SECRET
-  })]
+  providers: [
+    GitHub({
+      clientId: GITHUB_ID,
+      clientSecret: GITHUB_SECRET
+    }),
+    Google({
+      clientId: AUTH_GOOGLE_ID,
+      clientSecret: AUTH_GOOGLE_SECRET,
+    })]
 });
--- client/src/routes/login/+page.svelte
+++ client/src/routes/login/+page.svelte
@@ -2,8 +2,8 @@
   import { SignIn, SignOut } from "@auth/sveltekit/components";
   import { page } from "$app/stores";
 
-  if ($page.data.session) {
-    console.log("Session:", $page.data.session);
+  if ($page.data) {
+    console.log("Page Data:", $page.data);
   }
 </script>
 
@@ -14,5 +14,7 @@
   </SignOut>
 {:else}
   <h1>Log In</h1>
-  <SignIn provider="github" />
+  <SignIn>
+    <div slot="submitButton">Sign in</div>
+  </SignIn>
 {/if}
