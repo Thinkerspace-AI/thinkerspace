bca3c8c24063a81a7d362e38f7a9b9c023bcc1fd
Marius Barcenas <mariusulyzses.barcenas@gmail.com>
2024-05-14 18:20:02 +0800

    feat: add better navbar buttons logic

--- client/src/lib/components/NavBar.svelte
+++ client/src/lib/components/NavBar.svelte
@@ -27,11 +27,11 @@
   console.log("Page Data:", $page.data);
 </script>
 
-{#if $page.url.pathname != "/login"}
-  <nav class="fixed bg-Tpurple-100 w-full h-20 flex items-center justify-center">
-    <img class="ml-4 mr-auto h-16 w-fit" src={light_logo} alt="logo" />
-    <div class="ml-auto mr-4 w-fit">
-      <ul class="flex flex-row flex-end items-center justify-center">
+<nav class="fixed bg-Tpurple-100 w-full h-20 flex items-center justify-center">
+  <img class="ml-4 mr-auto h-16 w-fit" src={light_logo} alt="logo" />
+  <div class="ml-auto mr-4 w-fit">
+    <ul class="flex flex-row flex-end items-center justify-center">
+      {#if $page.data.session?.user}
         {#if $page.url.pathname != "/new"}
           <li class="text-Tyellow-100">
             <a
@@ -72,10 +72,26 @@
             on:click={openSignOutModal}>Sign Out</button
           >
         </li>
-      </ul>
-    </div>
-  </nav>
-{/if}
+      {:else if $page.url.pathname != "/login"}
+        <li class="text-Tyellow-100">
+          <a
+            href="/login"
+            class="bg-Tyellow-100 hover:bg-Tpurple-100 text-Tpurple-100 hover:text-Tyellow-100 font-inter py-2 px-3 my-2 mx-1 rounded-full w-fit"
+            >Sign In</a
+          >
+        </li>
+      {:else}
+        <li class="text-Tyellow-100">
+          <a
+            href="/login"
+            class="underline decoration-Tyellow-100 decoration-4 underline-offset-4 bg-Tpurple-100 hover:bg-Tyellow-100 text-Tyellow-100 hover:text-Tpurple-100 font-inter py-2 px-3 my-2 mx-1 rounded-full w-fit"
+            >Sign In</a
+          >
+        </li>
+      {/if}
+    </ul>
+  </div>
+</nav>
 
 <Modal bind:this={signOutModal}>
   <h1 class="font-bold font-inter text-2xl pb-1">Do you want to log out?</h1>
