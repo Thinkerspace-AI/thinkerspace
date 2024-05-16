26656332388169d4c11b9b88b48b915555d2fa58
Marius Barcenas <mariusulyzses.barcenas@gmail.com>
2024-05-17 03:05:14 +0800

    feat: add custom message if no sessions for user

--- client/src/routes/profile/+page.svelte
+++ client/src/routes/profile/+page.svelte
@@ -23,20 +23,24 @@
           <a href="/new">New</a>
         </button>
       </div>
-      {#each sessions as session}
-        <div class="flex h-fit flex-row items-center justify-center mb-3">
-          <div class="h-fit mr-40 w-full flex flex-col items-left justify-center">
-            <h1 class="font-bold font-inter text-2xl pb-1 pl-1">
-              <a href="/session/{session.session_id}">{session.title}</a>
-            </h1>
-            <h6 class="">{session.last_message}</h6>
+      {#if sessions.length === 0}
+        <p class="font-bold font-inter pb-1 pl-1">No sessions found. Click above to create one.</p>
+      {:else}
+        {#each sessions as session}
+          <div class="flex h-fit flex-row items-center justify-center mb-3">
+            <div class="h-fit mr-40 w-full flex flex-col items-left justify-center">
+              <h1 class="font-bold font-inter text-2xl pb-1 pl-1">
+                <a href="/session/{session.session_id}">{session.title}</a>
+              </h1>
+              <h6 class="">{session.last_message}</h6>
+            </div>
+            <button
+              class="bg-Tpurple-100 hover:bg-Tyellow-100 outline outline-offset-1 outline-Tpurple-100 text-Tyellow-100 hover:text-Tpurple-100 text-lg font-inter py-2 px-4 rounded-full w-fit h-fit"
+              tabindex="-1"><a href="/session/{session.session_id}">Open</a></button
+            >
           </div>
-          <button
-            class="bg-Tpurple-100 hover:bg-Tyellow-100 outline outline-offset-1 outline-Tpurple-100 text-Tyellow-100 hover:text-Tpurple-100 text-lg font-inter py-2 px-4 rounded-full w-fit h-fit"
-            tabindex="-1"><a href="/session/{session.session_id}">Open</a></button
-          >
-        </div>
-      {/each}
+        {/each}
+      {/if}
     </div>
   </div>
 </div>
